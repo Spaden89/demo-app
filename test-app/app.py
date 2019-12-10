40e5bd335de9dc6a44d9c0d72e46c8c2ad5f0c69
Daan <dlh.stolk@gmail.com>
2019-12-10 16:31:27 +0100

    final url for template

--- test-app/app.py
+++ test-app/app.py
@@ -140,7 +140,7 @@ def checkout_endpoint():
         # generate a checkout page
         amount = 1234 
         merchant_reference = "VF-001" 
-        return_url = host + url_for('checkout_template')
+        return_url = url_for('thankyou')
         # process transaction
         if request.form.get('process_transaction'):
             process_transaction = False
@@ -158,7 +158,7 @@ def checkout_endpoint():
             threeds_enabled = False  
         threeds_currency = "GBP"
         threeds_transaction_mode = "S"
-        template = api_host + "/checkout/template/v1"
+        template = host + url_for('checkout_template')
         # create checkout url
         checkout_json = dimebox.createCheckout(account, 
             amount, 
